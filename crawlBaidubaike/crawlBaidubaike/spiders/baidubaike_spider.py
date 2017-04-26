0871a1d0106dc0690a31950ae4c2307bd16e4823
wangruoxu <1026824311@qq.com>
2017-04-26 22:46:39 +0800

    modify getSeofangfaUrl,import module datetime;print scrapyCount in pipelines;solve synchronization between crawl nums and download nums

--- crawlBaidubaike/crawlBaidubaike/spiders/baidubaike_spider.py
+++ crawlBaidubaike/crawlBaidubaike/spiders/baidubaike_spider.py
@@ -52,7 +52,7 @@ class BaidubaikeSpider(scrapy.Spider):
             return
         baidubaikeItem['url'] = url
 
-        print time.ctime()+'-'*5+'crawl %d : %s' % (self.crawlingCount, url)
+        
 #title
         titleNode = response.xpath('.//dd[@class="lemmaWgt-lemmaTitle-title"]')
         h1 = titleNode.xpath('.//h1/text()').extract()[0]
@@ -64,99 +64,103 @@ class BaidubaikeSpider(scrapy.Spider):
         # print title
         # print '-'*20 + 'title' + '-'*20
         baidubaikeItem['title'] = title
-#content
-        paras = response.xpath('.//div[@class="para"]')
-        content = ''
-        if len(paras)!= 0:
-            for para in paras:
-                texts = para.xpath('.//text()').extract()
-                for text in texts:
-                    content += text
-        # print content
-        # print '-'*20 + 'content' + '-'*20
-            baidubaikeItem['content'] = content
-        else:
-            # print 'no content'
-            baidubaikeItem['content'] = None            
-#infobox
-        infobox = {}
-        infoboxNode = response.xpath('.//div[@class="basic-info cmn-clearfix"]')
-        if len(infoboxNode) != 0:
-            infobox_names = infoboxNode.xpath('.//dt//text()').extract()
-            infobox_valuesNode = infoboxNode.xpath('.//dd')
-            infobox_values = []
-            for i in infobox_valuesNode:
-                l = i.xpath('.//text()').extract()
-                value = ''
-                for j in l:
-                    value = value + j.strip()
-                infobox_values.append(value.encode('utf-8'))
-            for i in range(len(infobox_names)):
-                # print infobox_names[i] + ':' + infobox_values[i] 
-                # print unicode(infobox_names[i],'gbk').encode('utf-8')  + ':'
-                # print infobox_values[i].strip()
-                infobox[infobox_names[i].strip()] = infobox_values[i].strip()
-            baidubaikeItem['infobox'] = infobox
-        else:
-            # print 'no infobox'
-            baidubaikeItem['infobox'] = None
-#category
-        categoryNode  = response.xpath('//div[@class="lemmaWgt-lemmaCatalog"]')
-        if len(categoryNode) != 0:
-            category = categoryNode.xpath('.//a//text()').extract()
-            # print type(category)
-            baidubaikeItem['category'] = category
-            # print category
-            # print '-'*20 + 'category' + '-'*20
-        else :
-            # print 'no category'
-            baidubaikeItem['category'] = None
-#reference
-        reference = {}
-        referenceNode = response.xpath('//li[@class="reference-item "]//a[@rel="nofollow"]')
-        if len(referenceNode)!=0:
-            for i in referenceNode:
-                name = i.xpath('.//text()')[0].extract()
-                link = i.xpath('.//@href')[0].extract()
-                reference[name] = link
-                baidubaikeItem['reference'] = reference
-            # for k,v in reference.iteritems():
-            #     print k + ':' +v
-            # print '-'*20 + 'reference' +'-'*20
+        # print '-' * 5 + 'crawl %d :%s-----%s' % (self.crawlingCount, title, url)
+#main_content 
+        main_content =  response.xpath('.//div[@class="main-content"]').extract()
+        baidubaikeItem['main_content'] = main_content
+        baidubaikeItem['count'] = self.crawlingCount
+# #content
+#         paras = response.xpath('.//div[@class="para"]')
+#         content = ''
+#         if len(paras)!= 0:
+#             for para in paras:
+#                 texts = para.xpath('.//text()').extract()
+#                 for text in texts:
+#                     content += text
+#         # print content
+#         # print '-'*20 + 'content' + '-'*20
+#             baidubaikeItem['content'] = content
+#         else:
+#             # print 'no content'
+#             baidubaikeItem['content'] = None            
+# #infobox
+#         infobox = {}
+#         infoboxNode = response.xpath('.//div[@class="basic-info cmn-clearfix"]')
+#         if len(infoboxNode) != 0:
+#             infobox_names = infoboxNode.xpath('.//dt//text()').extract()
+#             infobox_valuesNode = infoboxNode.xpath('.//dd')
+#             infobox_values = []
+#             for i in infobox_valuesNode:
+#                 l = i.xpath('.//text()').extract()
+#                 value = ''
+#                 for j in l:
+#                     value = value + j.strip()
+#                 infobox_values.append(value.encode('utf-8'))
+#             for i in range(len(infobox_names)):
+#                 # print infobox_names[i] + ':' + infobox_values[i] 
+#                 # print unicode(infobox_names[i],'gbk').encode('utf-8')  + ':'
+#                 # print infobox_values[i].strip()
+#                 infobox[infobox_names[i].strip()] = infobox_values[i].strip()
+#             baidubaikeItem['infobox'] = infobox
+#         else:
+#             # print 'no infobox'
+#             baidubaikeItem['infobox'] = None
+# #category
+#         categoryNode  = response.xpath('//div[@class="lemmaWgt-lemmaCatalog"]')
+#         if len(categoryNode) != 0:
+#             category = categoryNode.xpath('.//a//text()').extract()
+#             # print type(category)
+#             baidubaikeItem['category'] = category
+#             # print category
+#             # print '-'*20 + 'category' + '-'*20
+#         else :
+#             # print 'no category'
+#             baidubaikeItem['category'] = None
+# #reference
+#         reference = {}
+#         referenceNode = response.xpath('//li[@class="reference-item "]//a[@rel="nofollow"]')
+#         if len(referenceNode)!=0:
+#             for i in referenceNode:
+#                 name = i.xpath('.//text()')[0].extract()
+#                 link = i.xpath('.//@href')[0].extract()
+#                 reference[name] = link
+#                 baidubaikeItem['reference'] = reference
+#             # for k,v in reference.iteritems():
+#             #     print k + ':' +v
+#             # print '-'*20 + 'reference' +'-'*20
 
-        else:
-            # print 'no reference'
-            baidubaikeItem['reference'] = None
+#         else:
+#             # print 'no reference'
+#             baidubaikeItem['reference'] = None
 
-#tag
-        tag = []
-        tagNode = response.xpath('//span[@class="taglist"]//text()').extract()
-        if len(tagNode) != 0:
-            for i in tagNode:
-                tag.append(i.strip())
-            baidubaikeItem['tag'] = tag
-            # print tag
-            # print '-'*20 + 'tag' +'-'*20
-        else:
-            # print 'no tag'
-            baidubaikeItem['tag'] = None
+# #tag
+#         tag = []
+#         tagNode = response.xpath('//span[@class="taglist"]//text()').extract()
+#         if len(tagNode) != 0:
+#             for i in tagNode:
+#                 tag.append(i.strip())
+#             baidubaikeItem['tag'] = tag
+#             # print tag
+#             # print '-'*20 + 'tag' +'-'*20
+#         else:
+#             # print 'no tag'
+#             baidubaikeItem['tag'] = None
 
 
-#image_url
-        image_urlNode = response.xpath('//div[@class="summary-pic"]//img//@src')
-        if len(image_urlNode) != 0:
-            image_url = image_urlNode[0].extract()
-            baidubaikeItem['image_url'] = image_url
-        else:
-            baidubaikeItem['image_url'] = None
-        # print image_url
-        # print '-'*20 + 'image_url' +'-'*20
+# #image_url
+#         image_urlNode = response.xpath('//div[@class="summary-pic"]//img//@src')
+#         if len(image_urlNode) != 0:
+#             image_url = image_urlNode[0].extract()
+#             baidubaikeItem['image_url'] = image_url
+#         else:
+#             baidubaikeItem['image_url'] = None
+#         # print image_url
+#         # print '-'*20 + 'image_url' +'-'*20
 
-#inner_related_urls
+# inner_related_urls
         urls = response.xpath("//a[contains(@href,'/item/')]/@href").extract()
         baidubaikeItem['inner_related_urls'] = urls
-        # print 'end collect'
-
+        print '*'*5+str(self.crawlingCount)+' : '+url
         yield baidubaikeItem
 
         # print '-----------------------------------------------------------------'
